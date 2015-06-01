a80cd7d78f2352101c540b1fe3f1305b7058593d
Christoph Wille <christoph.wille@gmail.com>
2015-06-01 19:21:04 +0200

    Simple port of the existing Web infrastructure to the new site

---
+++ WebSite/App_Code/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public static class Constants
+{
+    public static string VsixUrl = @"https://visualstudiogallery.msdn.microsoft.com/68c1575b-e0bf-420d-a94b-1b0f4bcdcbcc";
+}
