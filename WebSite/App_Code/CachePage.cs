2525f3cb627c0cfe64c0fe47594765643d795eb2
Christoph Wille <christoph.wille@gmail.com>
2015-06-22 14:10:50 +0200

    Add caching

---
+++ WebSite/App_Code/CachePage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public static class CachePage
+{
+    //
+    // Inspired by Mads' https://github.com/madskristensen/vswebessentials.com/blob/master/Website/Views/_Layout.cshtml
+    //
+    public static void WhenNotLocal(HttpContextBase context)
+    {
+        if (!context.Request.IsLocal)
+        {
+            context.Response.Cache.SetValidUntilExpires(true);
+            context.Response.Cache.SetExpires(DateTime.Now.AddDays(1));
+            context.Response.Cache.SetCacheability(HttpCacheability.Public);
+
+            string file = context.Server.MapPath(context.Request.CurrentExecutionFilePath);
+            context.Response.AddFileDependency(file);
+            context.Response.Cache.SetLastModifiedFromFileDependencies();
+        }
+    }
+}
