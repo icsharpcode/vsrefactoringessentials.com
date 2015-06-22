ff791d56024298e938f731af57b0b95bda634b9c
Christoph Wille <christoph.wille@gmail.com>
2015-06-22 10:26:45 +0200

    Work on the highlights of the main page (glyph icons not yet decided)

--- WebSite/App_Code/Constants.cs
+++ WebSite/App_Code/Constants.cs
@@ -6,4 +6,7 @@ using System.Web;
 public static class Constants
 {
     public static string VsixUrl = @"https://visualstudiogallery.msdn.microsoft.com/68c1575b-e0bf-420d-a94b-1b0f4bcdcbcc";
+    public static string NuGetUrl = @"https://www.nuget.org/packages/NR6Pack/";
+    public static string GithubUrl = @"https://github.com/icsharpcode/RefactoringEssentials";
+    public static string TwitterUrl = @"https://twitter.com/vsrefactoring";
 }
