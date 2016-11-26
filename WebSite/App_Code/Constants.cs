e44cadd21219e748178d35e03d3b8aa7472b07f1
Christoph Wille <christoph.wille@gmail.com>
2016-11-26 11:49:15 +0100

    Prepare for sim-shipping 2015/2017 versions of the extension

--- WebSite/App_Code/Constants.cs
+++ WebSite/App_Code/Constants.cs
@@ -5,7 +5,9 @@ using System.Web;
 
 public static class Constants
 {
-    public static string VsixUrl = @"https://visualstudiogallery.msdn.microsoft.com/68c1575b-e0bf-420d-a94b-1b0f4bcdcbcc";
+    public static string VsixUrl2015 = @"https://marketplace.visualstudio.com/items?itemName=SharpDevelopTeam.RefactoringEssentialsforVisualStudio";
+    public static string VsixUrl2017 = @"https://visualstudiogallery.msdn.microsoft.com/68c1575b-e0bf-420d-a94b-1b0f4bcdcbcc";
+
     public static string NuGetUrl = @"https://www.nuget.org/packages/RefactoringEssentials/";
     public static string GithubUrl = @"https://github.com/icsharpcode/RefactoringEssentials";
     public static string TwitterUrl = @"https://twitter.com/vsrefactoring";
