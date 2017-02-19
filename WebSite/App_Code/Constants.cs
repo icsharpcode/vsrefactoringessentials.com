402da43e5666299c531ffbb516179eccb184adaa
Christoph Wille <christoph.wille@gmail.com>
2017-02-19 19:01:40 +0100

    Release of 4.8 & 5.0

--- WebSite/App_Code/Constants.cs
+++ WebSite/App_Code/Constants.cs
@@ -5,8 +5,8 @@ using System.Web;
 
 public static class Constants
 {
-    public static string VsixUrl2015 = @"https://marketplace.visualstudio.com/items?itemName=SharpDevelopTeam.RefactoringEssentialsforVisualStudio";
-    public static string VsixUrl2017 = @"https://visualstudiogallery.msdn.microsoft.com/68c1575b-e0bf-420d-a94b-1b0f4bcdcbcc";
+    public static string VsixUrl2015 = @"https://marketplace.visualstudio.com/vsgallery/68c1575b-e0bf-420d-a94b-1b0f4bcdcbcc";
+    public static string VsixUrl2017 = @"https://marketplace.visualstudio.com/vsgallery/05ef5b4c-244a-4fc7-ab4a-a76f2ac5f027";
 
     public static string NuGetUrl = @"https://www.nuget.org/packages/RefactoringEssentials/";
     public static string GithubUrl = @"https://github.com/icsharpcode/RefactoringEssentials";
